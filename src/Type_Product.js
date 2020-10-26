13837affd5eba8c2858d034670502c277a8b0f90
unknown <kevin18patel@gmail.com>
2020-10-26 20:39:46 +0530

    a react website

--- src/Type_Product.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Type_Product = () => {
-    return (
-        <div className = "type_product">
-            <p>this is tyepr product page</p>
-        </div>
-    )
-}
