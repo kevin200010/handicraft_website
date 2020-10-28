12f3756a5a9d5f645ac62e092646bfb39d2cdbce
unknown <kevin18patel@gmail.com>
2020-10-28 09:23:46 +0530

    Updates

---
+++ precache-manifest.ebaad19a6c75e7629021608e906d031f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e47fbd83a9ee8b0e8ff065430aca01",
+    "url": "/handicraft_website/index.html"
+  },
+  {
+    "revision": "bcfb7221078c163b6589",
+    "url": "/handicraft_website/static/css/main.05ac1ade.chunk.css"
+  },
+  {
+    "revision": "0293409a65277dbcd3b6",
+    "url": "/handicraft_website/static/js/2.2b09772d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/handicraft_website/static/js/2.2b09772d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfb7221078c163b6589",
+    "url": "/handicraft_website/static/js/main.cde555b4.chunk.js"
+  },
+  {
+    "revision": "1d69a5645d075e9029bf",
+    "url": "/handicraft_website/static/js/runtime-main.204eadaf.js"
+  }
+]);
